476bd314cafdd9ac0f19ee1ede8e0f79b885350a
Ken Soh <ken@tebel.sg>
2016-10-14 08:13:29 +0200

    Update index.php

--- gist/index.php
+++ gist/index.php
@@ -9,7 +9,7 @@ $gist = [
 	"interface_automation" => "1d1722fb7feab973576a6e2a02a8da93",
 	"contact_virtual_reality" => "740659f1ad86831261d418e55f180a2b",
 	"digital-world_problems" => "e67a8c0048e918ce5bc102f8b85bd9ae",
-	"credits_and_attribution" => "ab7eb29a2e2fecae95df9b11abd9ba5d",
+	"credits_and_attribution" => "85c581db27bb2932ca35f9685598358a",
 ];
 
 // throw 404 error if input gist name is not found in above list
