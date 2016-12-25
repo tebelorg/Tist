d399791fa25813bcbe0e651eaf9db39067b92184
Ken Soh <ken@tebel.sg>
2016-12-25 12:26:59 +0800

    Add files via upload

--- gist/gist_map.php
+++ gist/gist_map.php
@@ -1,7 +1,7 @@
 <?php
 $gist = [
-"open-source_attribution" => "a678b5f0c06724b9079845f187b3d26f",
-"credits_and_attribution" => "85c581db27bb2932ca35f9685598358a",
+"open-source" => "a678b5f0c06724b9079845f187b3d26f",
+"open-source_attribution" => "85c581db27bb2932ca35f9685598358a",
 "digital-world_problems" => "e67a8c0048e918ce5bc102f8b85bd9ae",
 "interface_automation" => "1d1722fb7feab973576a6e2a02a8da93",
 ];
--- gist/index.php
+++ gist/index.php
@@ -9,8 +9,8 @@
 $gist = [
 	"interface_automation" => "1d1722fb7feab973576a6e2a02a8da93",
 	"digital-world_problems" => "e67a8c0048e918ce5bc102f8b85bd9ae",
-	"credits_and_attribution" => "85c581db27bb2932ca35f9685598358a",
-	"open-source_attribution" => "a678b5f0c06724b9079845f187b3d26f",
+	"open-source_attribution" => "85c581db27bb2932ca35f9685598358a",
+	"open-source" => "a678b5f0c06724b9079845f187b3d26f",
 ];
 $gist_found = FALSE;
 foreach ($gist as $gist_name => $gist_id) {if ($gist_name == $_GET['show']) $gist_found = TRUE;}
