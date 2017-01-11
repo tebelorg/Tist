8260fb276283bf531c48753b8eeffe99e544dd72
Ken Soh <ken@tebel.org>
2017-01-11 12:31:09 +0800

    Add files via upload

--- gist/gist_map.php
+++ gist/gist_map.php
@@ -1,8 +1,8 @@
 <?php
 $gist = [
 "open-source" => "a678b5f0c06724b9079845f187b3d26f",
-"open-source_attribution" => "85c581db27bb2932ca35f9685598358a",
-"digital-world_problems" => "e67a8c0048e918ce5bc102f8b85bd9ae",
+"open-source_software" => "85c581db27bb2932ca35f9685598358a",
 "interface_automation" => "1d1722fb7feab973576a6e2a02a8da93",
+"about" => "b2f3f7c2176cbabd42b6746ce8d258b1",
 ];
 ?>
